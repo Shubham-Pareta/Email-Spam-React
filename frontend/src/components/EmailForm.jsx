9482d6a79204a0ff628d4bf967aaa8725200f2ef
Shubham-Pareta <ishubhampareta@gmail.com>
2026-03-22 14:46:18 +0530

    add dist

--- frontend/src/components/EmailForm.jsx
+++ frontend/src/components/EmailForm.jsx
@@ -11,7 +11,7 @@ function EmailForm() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
